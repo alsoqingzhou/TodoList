1c65f257b7563e49c4f2b302d3ef8ef4cab7cdef
unknown <alsoqingzhou@gmail.com>
2020-07-04 22:15:58 +0800

    git commit -m"add a todolist app"

---
+++ src/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <input v-model="inputValue">
+    <button @click="handleSubmit">Submit</button>
+    <ul>
+      <todo-item 
+      v-for="(item, index) of list"
+      v-bind:key="index"
+      :content="item"
+      :position="index"
+      @delete="handleDelete"
+      >
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import TodoItem from './components/TodoItem'
+
+export default {
+  components: {
+    "todo-item": TodoItem
+  },
+  data () {
+    return{
+      inputValue:"",
+      list: [],
+    }
+  },
+  methods: {
+    handleSubmit (){
+      this.list.push(this.inputValue);
+      this.inputValue = '';
+    },
+
+    handleDelete (position){
+      this.list.splice(position, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props:['content','position'],
+  methods:{
+    handleClick (){
+      this.$emit('delete', this.position)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
